d3970eb49b2e00d0b822891a35ff0d7fdf92fd9e
Alexandre BRUHAT <apytio3@gmail.com>
2023-04-28 20:10:24 +0200

    First commit :rocket:

---
+++ skin.js
@@ -0,0 +1,45 @@
+// ==UserScript==
+// @name         New Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       You
+// @match        https://impeblast.youtrack.cloud/gantt-charts/173-0
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtrack.cloud
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    const versionLikeName = '';
+    const devLikeName = '';
+    const testLikeName = '';
+    const prodLikeName = '';
+
+    const searchXpathTemplate = '//div[.//div[@data-test="issue-summary" and contains(normalize-space(), "%%")] and @data-test="bar"]
+
+    const pathToAllVersionLikeSegment = searchXpathTemplate.replace('%%', 'V');
+    const pathToAllDevLikeSegment = searchXpathTemplate.replace('%%', 'Dev');
+    const pathToAllTestLikeSegment = searchXpathTemplate.replace('%%', 'Recette');
+    const pathToAllProdLikeSegment = searchXpathTemplate.replace('%%', 'MEP');
+
+
+    const versionStyle = 'background-color: blue;';
+    const devStyle = 'background-color: blue;';
+    const testStyle = 'background-color: blue;';
+    const prodStyle = 'background-color: blue;';
+
+    function addStyleToPath(xpath, style) {
+
+    }
+
+    addStyleToPath(pathToAllVersionLikeSegment, versionStyle);
+    addStyleToPath(pathToAllDevLikeSegment, devStyle);
+    addStyleToPath(pathToAllTestLikeSegment, testStyle);
+    addStyleToPath(pathToAllProdLikeSegment, prodStyle);
+
+    // Your code here...
+
+
+})();
